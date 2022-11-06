c1450a1489f1aec20dfb33157ed2fa66c28ab5cf
tahaozturkk <ozturktaha1453@gmail.com>
2022-11-06 18:04:47 +0300

    first commit

---
+++ eCommerceApplication/Services/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using eCommerceEntity.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApplication.Services
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        DbSet<T> Table { get; }
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> predicate); // (x = >x.Id == Id)
+        Task<T> GetSingle(Expression<Func<T, bool>> predicate);
+        Task<T> GetById(string id);
+        Task<bool> AddAsync(T model);
+        Task<bool> AddRangeAsync(List<T> models);
+        bool Delete(T model);
+        bool DeleteRange(List<T> models);
+        Task<bool> DeleteAsync(string id);
+        bool Update(T model);
+        Task<int> SaveAsync();
+    }
+
+}
---
+++ eCommerceDataAccess/Context/eCommerceDbContext.cs
@@ -0,0 +1,18 @@
+﻿using eCommerceEntity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceDataAccess.Context
+{
+    public class eCommerceDbContext : IdentityDbContext<AppUser>
+    {
+        public eCommerceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
